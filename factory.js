05e15c224c1d6fb1eb61609f903628405a3b0c3f
balwant-singh <singh.balwant1104@gmail.com>
2017-12-04 23:24:25 +0530

    demo changes

--- factory.js
+++ factory.js
@@ -1,12 +1,22 @@
 var fs = require('fs');
+var invoicesJson = require('./public/invoices');
 var factory = {};
 
 factory._getInvoices = function(folderName) {
     var invoices = [];
     fs.readdirSync(folderName).forEach(file => {
         var data = {parentPath: folderName, fileName: file}
+        for(var i = 0; i < invoicesJson.length; i++) {
+            if(invoicesJson[i].file == file) {
+                data.id = invoicesJson[i].id;
+                break;
+            }
+        }
         invoices.push(data);
     })
+
+    
+
     return invoices;    
 },
 
